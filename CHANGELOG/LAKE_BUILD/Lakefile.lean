3767fec2bf18a96d79d4d0c440f122e5b80f557d
quantarion369-arch <quantarion369@gmail.com>
2026-09-05 19:59:28 -0400

    Add initial Lakefile for AQARION package

---
+++ CHANGELOG/LAKE_BUILD/Lakefile.lean
@@ -0,0 +1,8 @@
+import Lake
+open Lake DSL
+
+package AQARION where
+  -- add package configuration options here
+
+require Mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
